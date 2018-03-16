a1cd11c9f172f242ced2f941b5bb96880f51b90e
Krishnan Kannan <kxkannan@gmail.com>
2018-03-15 18:45:38 -0600

    fixed the browser refresh and comment count.

--- frontend/src/components/NotFoundPage.js
+++ frontend/src/components/NotFoundPage.js
@@ -1,17 +1,30 @@
 import React, {Component} from 'react'
 import { Link } from 'react-router-dom'
+import {connect} from 'react-redux'
+
 
 
 class NotFoundPage extends Component {
 
     render() {
-        return (
-            <div>
-                <h3>Sorry, the page you are looking for is not found</h3>
-                <p><Link to="/">Back to Home</Link></p>
-            </div>
-        )
+        if (!this.props.selectedPostId) {
+            return (
+                <div>
+                    <h3>Sorry, the page you are looking for is not found</h3>
+                    <p><Link to="/">Back to Home</Link></p>
+                </div>
+            )
+        }
+        else {
+            return null
+        }
     }
 }
 
-export default NotFoundPage
+function mapStateToProps(posts) {
+    return {
+       selectedPostId: posts.posts.selectedPostId
+    }
+}
+
+export default connect(mapStateToProps)(NotFoundPage)
